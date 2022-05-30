0d367410a9dd6d80d23230814373fa46a6b93419
Yan Okumoto <yanokumotokanashiro@gmail.com>
2022-05-30 10:25:30 -0400

    Corrigindo a Activity e definindo ela como MainActivity

--- app/src/main/java/br/com/compass/compassmart/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/compass/compassmart/ui/activity/MainActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.compass.compassmart.R
 
-class OnboardingActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_onboarding)
+        setContentView(R.layout.activity_main)
     }
 }
