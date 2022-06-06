1dcab8ecf05b1e9d08f74d9c392b84f1e4729bda
Isadora Picoi <isadora.freitas01@gmail.com>
2022-06-06 11:20:28 -0400

    criando tela de login

---
+++ app/src/main/java/br/com/compass/compassmart/ui/LoginFragment.kt
@@ -0,0 +1,18 @@
+package br.com.compass.compassmart.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import br.com.compass.compassmart.R
+
+class LoginFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+}
