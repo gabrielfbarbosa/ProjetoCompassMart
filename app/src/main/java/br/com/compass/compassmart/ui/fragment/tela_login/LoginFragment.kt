5b1ea5a03b81327dff0f14845f929728de6ebfa2
Isadora Picoi <isadora.freitas01@gmail.com>
2022-06-14 10:20:35 -0400

    Alterações para fazer merge com develop

--- app/src/main/java/br/com/compass/compassmart/ui/fragment/tela_login/LoginFragment.kt
+++ app/src/main/java/br/com/compass/compassmart/ui/fragment/tela_login/LoginFragment.kt
@@ -1,4 +1,4 @@
-package br.com.compass.compassmart.ui
+package br.com.compass.compassmart.ui.fragment.tela_login
 
 import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
