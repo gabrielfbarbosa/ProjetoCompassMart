d1b914cea400fb70e3451316230a1a7ec24317d2
garielfbarbosa <gfelipebarbosa@yhoo.com>
2022-06-14 00:08:34 -0300

    corrigindo conflitos para o PR

--- app/src/main/java/br/com/compass/compassmart/ui/onboarding/OnboardingFragment.kt
+++ app/src/main/java/br/com/compass/compassmart/ui/onboarding/OnboardingFragment.kt
@@ -19,7 +19,7 @@ class OnboardingFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentOnboardingBinding.inflate(inflater, container, false)
         return binding.root
     }
