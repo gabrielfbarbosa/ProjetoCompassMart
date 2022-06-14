c955b7757b03a7fb8bf5bee14eec7eb972960233
Yan Okumoto <yanokumotokanashiro@gmail.com>
2022-06-14 10:55:27 -0400

    Atualizando os outros arquivos da branch

---
+++ app/src/main/java/br/com/compass/compassmart/ui/fragment/tela_onboarding/OnboardingAdapter.kt
@@ -0,0 +1,39 @@
+package br.com.compass.compassmart.ui.fragment.tela_onboarding
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import br.com.compass.compassmart.databinding.ItemOnboardingBinding
+import br.com.compass.compassmart.ui.fragment.tela_onboarding.model.InformacoesModel
+
+class OnboardingAdapter(
+    private val data: List<InformacoesModel>
+) : RecyclerView.Adapter<OnboardingAdapter.PaginasViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): OnboardingAdapter.PaginasViewHolder {
+        val view = ItemOnboardingBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PaginasViewHolder(view.root)
+    }
+
+    override fun onBindViewHolder(holder: OnboardingAdapter.PaginasViewHolder, position: Int) {
+        holder.bindInformacoes(data[position])
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    class PaginasViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val binding: ItemOnboardingBinding = ItemOnboardingBinding.bind(itemView)
+
+        fun bindInformacoes(informacoesModel: InformacoesModel) {
+            binding.informacaoInicialTitulo.text = informacoesModel.tiulo
+            binding.informacaoInicialDescricao.text = informacoesModel.descricao
+            binding.informacaoInicialImagem.setImageResource(informacoesModel.drawableId)
+        }
+    }
+}
