d9a7c90b664cb531626f4d2cdd69467953f98b9b
garielfbarbosa <gfelipebarbosa@yhoo.com>
2022-06-17 12:55:11 -0300

    Seleção do metodo de pagamento funcionando

--- app/src/main/java/br/com/compass/compassmart/ui/fragment/tela_meiopagamento/MeioPagamento.kt
+++ app/src/main/java/br/com/compass/compassmart/ui/fragment/tela_meiopagamento/MeioPagamento.kt
@@ -1,5 +1,6 @@
 package br.com.compass.compassmart.ui.fragment.tela_meiopagamento
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,7 +11,7 @@ import br.com.compass.compassmart.R
 import br.com.compass.compassmart.databinding.FragmentMeioPagamentoBinding
 import br.com.compass.compassmart.databinding.FragmentOnboardingBinding
 
-class MeioPagamento : Fragment() {
+class MeioPagamento : Fragment(), View.OnClickListener{
 
 
     private var _binding: FragmentMeioPagamentoBinding? = null
@@ -27,5 +28,22 @@ class MeioPagamento : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.cardCompassPay.setOnClickListener(this)
+        binding.cardPix.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        var escolha: Int //Para saber qual metodo de pagamento foi selecionado
+
+        if(v.id == binding.cardCompassPay.id){
+            binding.constraintLayoutCompassPay.setBackgroundResource(R.color.orange_500)
+            binding.constraintLayoutPix.setBackgroundResource(R.color.white)
+            escolha = 0
+        }
+        if(v.id == binding.cardPix.id){
+            binding.constraintLayoutCompassPay.setBackgroundResource(R.color.white)
+            binding.constraintLayoutPix.setBackgroundResource(R.color.orange_500)
+            escolha = 1
+        }
     }
 }
