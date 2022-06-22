a6b6b143cdbd06d9b793b3c2473972f50845ecae
Isadora Picoli <isadora.freitas01@gmail.com>
2022-06-22 04:34:46 -0400

    Adicionando a navegação da tela de cadastro

--- app/src/main/java/br/com/compass/compassmart/ui/fragment/tela_cadastro/CadastroFragment.kt
+++ app/src/main/java/br/com/compass/compassmart/ui/fragment/tela_cadastro/CadastroFragment.kt
@@ -1,6 +1,38 @@
 package br.com.compass.compassmart.ui.fragment.tela_cadastro
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import br.com.compass.compassmart.R
+import br.com.compass.compassmart.databinding.FragmentCadastroBinding
 
 class CadastroFragment : Fragment() {
+    private var _binding: FragmentCadastroBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCadastroBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.fragmentCadastroTxtInputNome.requestFocus()
+
+        binding.fragmentCadastroBtnCadastrar.setOnClickListener{
+            NavHostFragment.findNavController(this@CadastroFragment)
+                .navigate(R.id.action_CadastroFragment_to_CarrinhoComprasFragment)
+        }
+
+        binding.fragmentCadastroTxtLogin.setOnClickListener{
+            NavHostFragment.findNavController(this@CadastroFragment)
+                .navigate(R.id.action_CadastroFragment_to_LoginFragment)
+        }
+    }
 }
