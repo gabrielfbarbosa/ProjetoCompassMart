d96de8f05cce6d82cec044438b6c3251c23286c3
Yan Okumoto <yanokumotokanashiro@gmail.com>
2022-06-24 01:31:21 -0400

    Finalizando Tela de Endereço

--- app/src/main/java/br/com/compass/compassmart/ui/fragment/tela_endereco/EnderecoFragment.kt
+++ app/src/main/java/br/com/compass/compassmart/ui/fragment/tela_endereco/EnderecoFragment.kt
@@ -1,17 +1,18 @@
 package br.com.compass.compassmart.ui.fragment.tela_endereco
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
 import br.com.compass.compassmart.R
 import br.com.compass.compassmart.databinding.FragmentEnderecoBinding
-import br.com.compass.compassmart.databinding.FragmentProdutosBinding
 
 class EnderecoFragment : Fragment() {
 
-    private var _binding: FragmentProdutosBinding? = null
+    private var _binding: FragmentEnderecoBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -20,5 +21,27 @@ class EnderecoFragment : Fragment() {
     ): View {
         _binding = FragmentEnderecoBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.fragmentEnderecoEdtxtLogradouro.requestFocus()
+
+        binding.fragmentEnderecoBtnContinuar.setOnClickListener {
+
+            if (binding.fragmentEnderecoEdtxtLogradouro.editText?.text.toString().isEmpty() ||
+                binding.fragmentEnderecoEdtxtCep.editText?.text.toString().isEmpty() ||
+                binding.fragmentEnderecoEdtxtNumero.editText?.text.toString().isEmpty() ||
+                binding.fragmentEnderecoEdtxtBairro.editText?.text.toString().isEmpty() ||
+                binding.fragmentEnderecoEdtxtCidade.editText?.text.toString().isEmpty() ||
+                binding.fragmentEnderecoEdtxtUf.editText?.text.toString().isEmpty()
+            ) {
+                Toast.makeText(requireActivity(), "Preencha todos os campos!", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                NavHostFragment.findNavController(this@EnderecoFragment)
+                    .navigate(R.id.action_enderecoFragment_to_meioPagamento)
+            }
+        }
     }
 }
