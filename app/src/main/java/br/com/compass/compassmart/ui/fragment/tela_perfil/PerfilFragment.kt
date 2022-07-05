94381648eb574039238426111a90b698d3771699
Isadora Picoli <isadora.freitas01@gmail.com>
2022-07-05 11:37:45 -0400

    Criando tela de perfil

---
+++ app/src/main/java/br/com/compass/compassmart/ui/fragment/tela_perfil/PerfilFragment.kt
@@ -0,0 +1,27 @@
+package br.com.compass.compassmart.ui.fragment.tela_perfil
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import br.com.compass.compassmart.databinding.FragmentPerfilBinding
+
+class PerfilFragment : Fragment() {
+    private lateinit var _binding: FragmentPerfilBinding
+    private val viewModel : PerfilViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPerfilBinding.inflate(inflater, container, false)
+        return _binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
---
+++ app/src/main/java/br/com/compass/compassmart/ui/fragment/tela_perfil/PerfilViewModel.kt
@@ -0,0 +1,7 @@
+package br.com.compass.compassmart.ui.fragment.tela_perfil
+
+import androidx.lifecycle.ViewModel
+
+class PerfilViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
