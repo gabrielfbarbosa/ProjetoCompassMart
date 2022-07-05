98024ba6ea9df0601618903cb5630d755767c506
yuukidesu9 <joaoinstaller@gmail.com>
2022-07-05 10:07:46 -0400

    Implementação inicial da tela de categorias

---
+++ app/src/main/java/br/com/compass/compassmart/ui/fragment/tela_categorias/TelaCategoriasFragment.kt
@@ -0,0 +1,70 @@
+package br.com.compass.compassmart.ui.fragment.tela_categorias
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import br.com.compass.compassmart.R
+import br.com.compass.compassmart.databinding.FragmentTelaPrincipalBinding
+import br.com.compass.compassmart.ui.Produto
+
+class TelaCategoriasFragment : Fragment() {
+    private var _binding: FragmentTelaPrincipalBinding? = null
+    private val binding get() = _binding!!
+    private val produtos = mutableListOf<Produto>(
+        Produto(
+            "Galaxy A52 5G",
+            "",
+            "R$ 1779,00",
+            R.drawable.produto_galaxy_a52
+        ), Produto(
+            "Galaxy A52 5G",
+            "",
+            "R$ 1779,00",
+            R.drawable.produto_galaxy_a52
+        ), Produto(
+            "Galaxy A52 5G",
+            "",
+            "R$ 1779,00",
+            R.drawable.produto_galaxy_a52
+        ), Produto(
+            "Galaxy A52 5G",
+            "",
+            "R$ 1779,00",
+            R.drawable.produto_galaxy_a52
+        ), Produto(
+            "Galaxy A52 5G",
+            "",
+            "R$ 1779,00",
+            R.drawable.produto_galaxy_a52
+        ), Produto(
+            "Galaxy A52 5G",
+            "",
+            "R$ 1779,00",
+            R.drawable.produto_galaxy_a52
+        )
+    )
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentTelaPrincipalBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.fragmentTelaPrincipalRecyclerViewSelecionados.layoutManager = GridLayoutManager(requireContext(), 2)
+//        binding.fragmentTelaPrincipalRecyclerView.adapter = TelaPrincipalAdapter(produtos,
+//            object : TelaPrincipalAdapter.MeuOnClickListener {
+//                override fun onClick(produto: Produto) {
+//                    findNavController().navigate(TelaPrincipalFragmentDirections.actionProdutosFragmentToDetalhesDoProdutoFragment(
+//                        produto))
+//                }
+//            }
+//        )
+    }
+}
