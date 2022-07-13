3e0080a01a4b4603e8b41d85bccfce5f741ee55d
Isadora Picoli <isadora.freitas01@gmail.com>
2022-07-13 14:15:28 -0400

    Criando botão de logout

--- app/src/main/java/br/com/compass/compassmart/ui/fragment/tela_perfil/PerfilFragment.kt
+++ app/src/main/java/br/com/compass/compassmart/ui/fragment/tela_perfil/PerfilFragment.kt
@@ -1,27 +1,48 @@
 package br.com.compass.compassmart.ui.fragment.tela_perfil
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import br.com.compass.compassmart.R
 import br.com.compass.compassmart.databinding.FragmentPerfilBinding
 
 class PerfilFragment : Fragment() {
     private lateinit var _binding: FragmentPerfilBinding
-    private val viewModel : PerfilViewModel by viewModels()
+    private val viewModel: PerfilViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentPerfilBinding.inflate(inflater, container, false)
+        //requireActivity().actionBar?.show()
+        (requireActivity() as AppCompatActivity).supportActionBar?.title = "Perfil"
         return _binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-    }
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.fragment_perfil_menu, menu)
+            }
 
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.menu_sair -> {
+                        //logout()
+                        true
+                    }
+                    else -> false
+                }
+
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
 }
